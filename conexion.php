2754a35b682a8f11ad69dd5d7d71f2d89c0a4d42
bupitt <139922710+bupitt@users.noreply.github.com>
2024-06-13 08:33:29 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "localhost";
+$usuario = "username";
+$clave = "password";
+$bd = "candycandy";
+$port = "3307"
+$coneccion = mysqli_connect ($servidor, $usuario, $clave, $bd, $port);
+ ?>
